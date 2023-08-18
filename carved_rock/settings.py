df499a33d972d4c305a0ed2149c9b1662de884bb
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-18 18:28:24 +0200

    Created store app. Created the Product model. Added debug_toolbar to settings and urlpatterns in the projects urls file

--- carved_rock/settings.py
+++ carved_rock/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'carved_rock.urls'
