2e8c996c292bce12c496802f72f99b3168ef9c7e
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-20 01:32:59 +0200

    After customizing the model behaviour: the meta class, custom methods & managers, model inheritance

--- store/models.py
+++ store/models.py
@@ -1,12 +1,30 @@
+from decimal import Decimal
+from typing import Iterable, Optional
+
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+
+class ProductInStockQuerySet(models.QuerySet):
+    def in_stock(self):
+        return self.filter(stock_count__gt=0)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    stock_count = models.IntegerField(help_text="How many items are currently in stock.")
+    # stock_count = models.IntegerField(help_text="How many items are currently in stock.")
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(default="", blank=True)
     sku = models.CharField(max_length=20, unique=True, verbose_name="stock keeping unit")
+    # slug = models.SlugField()
+
+    in_stock = ProductInStockQuerySet.as_manager()
+
+    # def save(self, *args, **kwargs) -> None:
+    #     if not self.slug:
+    #         self.slug = slugify(self.name)
+    #     return super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['price', 'name']
@@ -14,8 +32,23 @@ class Product(models.Model):
             models.CheckConstraint(check=models.Q(price__gte=0), name="price_not_negative")
         ]
 
+    def get_absolute_url(self):
+        return reverse("store:product-detail", kwargs={"pk": self.id})
+
+    @property
+    def vat(self):
+        return Decimal(.2) * self.price
+
     def __str__(self):
         return self.name
+
+
+class DigitalProduct(Product):
+    file = models.FileField()
+
+
+class PhysicalProduct(Product):
+    stock_count = models.IntegerField(help_text="How many items are currently in stock.")
 
 
 class ProductImage(models.Model):  # one-to-many relationship
