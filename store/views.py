627c44009e8edafdccd011fe7fec2aa097d93a5f
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-19 12:44:01 +0200

    After checking models classes and instances, model fields, and managers and querysets

--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def category_view(request, name):
+    products = Product.objects.filter(category__name=name)
+    return render(
+        request,
+        "store/category.html",
+        {'products': products, 'category_name': name}
+    )
